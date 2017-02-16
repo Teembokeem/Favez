ec0be835716e33db1497e27f2a2c729760ed1a37
Teembokeem <myungtkim@gmail.com>
2017-02-15 17:10:52 -0800

    add render and render children methods for favorite view with all child components

--- src/modules/favorite/FavoriteView.js
+++ src/modules/favorite/FavoriteView.js
@@ -1,19 +1,59 @@
 import React, {PropTypes} from 'react';
+import * as FavoriteState from './FavoriteState';
 import {
   Text,
   View,
+  ScrollView,
   StyleSheet
 } from 'react-native';
+import {Actions} from 'react-native-router-flux';
+
+import FavoriteHeader from '../../components/favorite/favoriteHeader/favoriteHeader';
+import Header from '../../components/globals/header/Header';
+import HeaderTabs from '../../components/globals/headerTabs/headerTabs';
+import List from '../../components/globals/list/List';
 
 const FavoriteView = React.createClass({
   propTypes: {},
+  renderChildren() {
+    console.log('your props', this.props.selected);
+    switch (this.props.selected) {
+      case 'your lists':
+        console.log('sending lists');
+        return (
+          this.props.lists.map((list, index) => (
+              <List
+                list={list}
+                key={index}
+              >
+              </List>
+          ))
+        );
+      default :
+        return null;
+    }
+  },
 
   render() {
+    const child = this.renderChildren();
+    // const {faves, lists} = this.props;
+    const selectedTab = this.props.selected;
     return (
       <View style={styles.container}>
-        <Text>
-          Favorite
-        </Text>
+        <ScrollView
+        >
+          <FavoriteHeader />
+          <Header title={'FAVEZ'} />
+          <HeaderTabs
+            selected={selectedTab}
+            tabs={['your lists', 'collabs', 'liked']}
+          />
+          <View
+            style={styles.contentContainer}
+          >
+            {child}
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -23,6 +63,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  contentContainer: {
+    backgroundColor: '#e9e9e9',
+    padding: 10,
+    paddingTop: 15,
     alignItems: 'center'
   }
 });
