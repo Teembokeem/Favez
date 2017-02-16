36e24c840e01aee8a8a999f202ed295b559271e1
Teembokeem <myungtkim@gmail.com>
2017-02-15 17:08:46 -0800

    add custom header for favorite state

---
+++ src/components/favorite/favoriteHeader/favoriteHeader.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {Actions} from 'react-native-router-flux';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 30 : 0;
+
+const FavoriteHeader = React.createClass({
+
+  openFavoriteModal() {
+    Actions.FavoriteModal();
+  },
+
+  render() {
+    return (
+    <View style={[styles.feedNavHeader]}>
+        <View
+          style={styles.flex1}
+        >
+          <TouchableOpacity
+              style={styles.headerLeftButton}
+          >
+              <IoniconIcon style={styles.headerLeftButtonIcon} name="md-list-box"/>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={styles.flex2}
+        >
+          <TouchableOpacity
+              style={styles.headerRightButton}
+          >
+              <EntypoIcon style={styles.headerRightButtonIcon} name="plus"/>
+          </TouchableOpacity>
+        </View>
+    </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  feedNavHeader: {
+    // alignItems: 'center',
+    backgroundColor: Platform.OS === 'ios' ? '#FFFFFF' : '#FFFFFF',
+    // borderBottomColor: 'rgba(0, 0, 0, .15)',
+    // borderBottomWidth: Platform.OS === 'ios' ? StyleSheet.hairlineWidth : 0,
+    elevation: 4,
+    flex: 1,
+    width: 375,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingTop: STATUSBAR_HEIGHT
+    // height: APPBAR_HEIGHT + STATUSBAR_HEIGHT * 2
+  },
+  flex1: {
+    height: 35,
+    flex: 1,
+    paddingLeft: 20
+  },
+  headerLeftButtonIcon: {
+    width: 35,
+    fontSize: 30,
+    color: '#000000',
+    alignSelf: 'flex-start'
+  },
+  flex2: {
+    flex: 1,
+    paddingRight: 20
+  },
+  headerRightButtonIcon: {
+    height: 35,
+    width: 35,
+    fontSize: 35,
+    // top: 30,
+    // margin: 10,
+    color: '#000000',
+    alignSelf: 'flex-end'
+  },
+  placeHolder: {
+    marginLeft: 20
+  }
+
+});
+
+export default FavoriteHeader;
