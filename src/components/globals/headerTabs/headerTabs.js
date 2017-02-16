24479370911676cdd007adc03519aa75b1df8aab
Teembokeem <myungtkim@gmail.com>
2017-02-15 17:09:49 -0800

    create header tab global component

---
+++ src/components/globals/headerTabs/headerTabs.js
@@ -0,0 +1,58 @@
+import React from 'react';
+// import TabBarButton from '../components/TabBarButton';
+
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+function HeaderTabs({selected, tabs}) {
+  console.log("yoooooo vars", selected, tabs)
+  return (
+    <View style={[styles.HeaderTabs]}>
+      {tabs.map((tab, index) => {
+        return (
+          <View
+            style={styles.container}
+            key={'tab ' + index}
+          >
+            <Text
+              style={[styles.HeaderTabsTab, {color: tab === selected ? 'black' : '#d8d8d8'}]}
+            >
+            {tab.toUpperCase()}
+            </ Text>
+          </ View>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  HeaderTabs: {
+    width: 375,
+    padding: 6,
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    borderColor: '#efefef'
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  HeaderTabsTab: {
+    fontFamily: 'Hind-Bold',
+    fontSize: 18,
+    // paddingBottom: 5
+  },
+  HeaderTabsSeparator: {
+    textAlign: 'center',
+    backgroundColor: '#efefef',
+    borderRadius: 10,
+    height: 8,
+    width: 60
+  }
+});
+
+export default HeaderTabs;
