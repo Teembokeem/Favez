3bf931b0bb999b67d35c0d96960280f98f140dc5
Teembokeem <myungtkim@gmail.com>
2017-02-17 06:14:22 -0800

    add boilerplate for profile view

--- src/modules/profile/ProfileView.js
+++ src/modules/profile/ProfileView.js
@@ -2,18 +2,54 @@ import React, {PropTypes} from 'react';
 import {
   Text,
   View,
+  ScrollView,
   StyleSheet
 } from 'react-native';
+
+import ProfileHeader from '../../components/profile/profileHeader/profileHeader';
+import ProfileSummary from '../../components/profile/profileSummary/profileSummary';
+import ProfileActions from '../../components/profile/profileActions/profileActions';
+import HeaderTabs from '../../components/globals/headerTabs/headerTabs';
+import Card from '../../components/globals/card/card';
+import List from '../../components/globals/list/list';
 
 const ProfileView = React.createClass({
   propTypes: {},
 
+  renderChildren() {
+    switch (this.props.selected) {
+      case 'lists':
+        return (
+          this.props.lists.map((list, index) => (
+            <List
+              list={list}
+              key={'list ' + index}
+            >
+
+            </List>
+          ))
+        );
+      default :
+        return null;
+    }
+  },
+
   render() {
+    const child = this.renderChildren();
+    const selectedTab = this.props.selected;
     return (
       <View style={styles.container}>
-        <Text>
-          Profile View
-        </Text>
+        <ScrollView>
+          <ProfileHeader />
+          <ProfileSummary />
+          <ProfileActions />
+          <HeaderTabs
+            setFilter={this.setFilter}
+            selected={selectedTab}
+            tabs={['lists', 'collabs', 'subscriptions', 'likes', 'comments']}
+          />
+          {child}
+        </ScrollView>
       </View>
     );
   }
