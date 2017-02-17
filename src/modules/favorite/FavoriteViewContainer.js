477f90931da918cf92d5e6160019dc0c42e3451b
Teembokeem <myungtkim@gmail.com>
2017-02-17 08:50:07 -0800

    add view container filtered lists

--- src/modules/favorite/FavoriteViewContainer.js
+++ src/modules/favorite/FavoriteViewContainer.js
@@ -1,11 +1,18 @@
 import {connect} from 'react-redux';
 import FavoriteView from './FavoriteView';
-
 export default connect(
   state => {
+    const filteredLists = state.getIn(['favorite', 'lists']).get('data').toJS().filter((list) => {
+      if (state.getIn(['favorite', 'selected']) === 'your lists') {
+        return state.getIn(['favorite', 'user', 'data']).toJS().username === list.creator.name;
+      } else {
+        return state.getIn(['favorite', 'user', 'data']).toJS().username !== list.creator.name;
+      }
+    });
     return {
-      lists: state.getIn(['favorite', 'lists']).get('data').toJS(),
-      faves: state.getIn(['favorite', 'faves']).get('data').toJS(),
+      lists: filteredLists,
+      user: state.getIn(['profile', 'user', 'data']).toJS(),
+      favez: state.getIn(['favorite', 'favez']).get('data').toJS(),
       selected: state.getIn(['favorite', 'selected']),
       loading: state.getIn(['search', 'loading'])
     };
