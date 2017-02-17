d30029a5d17072363d839c13a683059a882a3bc7
Teembokeem <myungtkim@gmail.com>
2017-02-17 06:14:33 -0800

    add view container connections for profile view

--- src/modules/profile/ProfileViewContainer.js
+++ src/modules/profile/ProfileViewContainer.js
@@ -1,4 +1,26 @@
 import {connect} from 'react-redux';
 import ProfileView from './ProfileView';
 
-export default connect()(ProfileView);
+
+function findSubset(subSetType) {
+  switch (subSetType) {
+    case 'alerts':
+      return ['follow', 'unfollow', 'invitation_accept', 'invitation_decline', 'comment', 'favez_like', 'new_user'];
+    case 'invitations':
+      return ['invitation_request'];
+    default:
+      return null;
+  }
+}
+
+export default connect(
+  state => {
+    return {
+      user: state.getIn(['profile', 'user', 'data']).toJS(),
+      lists: state.getIn(['profile', 'lists']).get('data').toJS(),
+      faves: state.getIn(['profile', 'faves']).get('data').toJS(),
+      selected: state.getIn(['profile', 'selected']),
+      loading: state.getIn(['profile', 'loading'])
+    };
+  }
+)(ProfileView);
