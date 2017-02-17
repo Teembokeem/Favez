f49889366c070132705bf7614dfaaa65b8cff21c
Teembokeem <myungtkim@gmail.com>
2017-02-17 07:51:24 -0800

    add filtered list functionality for profile container

--- src/modules/profile/ProfileViewContainer.js
+++ src/modules/profile/ProfileViewContainer.js
@@ -2,12 +2,10 @@ import {connect} from 'react-redux';
 import ProfileView from './ProfileView';
 
 
-function findSubset(subSetType) {
+function findSubset(state, subSetType) {
   switch (subSetType) {
-    case 'alerts':
-      return ['follow', 'unfollow', 'invitation_accept', 'invitation_decline', 'comment', 'favez_like', 'new_user'];
-    case 'invitations':
-      return ['invitation_request'];
+    case 'lists':
+      return state.getIn(['profile', 'user', 'data']).toJS().username;
     default:
       return null;
   }
@@ -15,9 +13,16 @@ function findSubset(subSetType) {
 
 export default connect(
   state => {
+    const filteredLists = state.getIn(['profile', 'lists']).get('data').toJS().filter((list) => {
+      if (state.getIn(['profile', 'selected']) === 'lists') {
+        return state.getIn(['profile', 'user', 'data']).toJS().username === list.creator.name;
+      } else {
+        return state.getIn(['profile', 'user', 'data']).toJS().username !== list.creator.name;
+      }
+    });
     return {
       user: state.getIn(['profile', 'user', 'data']).toJS(),
-      lists: state.getIn(['profile', 'lists']).get('data').toJS(),
+      lists: filteredLists,
       faves: state.getIn(['profile', 'faves']).get('data').toJS(),
       selected: state.getIn(['profile', 'selected']),
       loading: state.getIn(['profile', 'loading'])
