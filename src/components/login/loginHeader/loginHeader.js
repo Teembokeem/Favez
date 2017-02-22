e783afcd1f509e0df420c9bf83130cf04d4247e4
Teembokeem <myungtkim@gmail.com>
2017-02-21 16:09:19 -0800

    add login header component from tim-login

---
+++ src/components/login/loginHeader/loginHeader.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Actions} from 'react-native-router-flux';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
+// import HeaderBack from '../../headerBack/headerBack';
+
+
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 30 : 0;
+
+const LoginHeader = React.createClass({
+
+  openFavoriteModal() {
+    Actions.FavoriteModal();
+  },
+
+  render() {
+    console.log('sup back', this.props)
+    return (
+    <View style={[styles.feedNavHeader]}>
+        <View
+          style={styles.flex1}
+        >
+          <TouchableOpacity onPress={this.props.back} style={styles.headerBackButton} >
+            <IoniconIcon style={styles.backButtonIcon} name='md-arrow-round-back'/>
+          </TouchableOpacity>
+        </View>
+    </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  feedNavHeader: {
+    // alignItems: 'center',
+    backgroundColor: Platform.OS === 'ios' ? '#FFFFFF' : '#FFFFFF',
+    // borderBottomColor: 'rgba(0, 0, 0, .15)',
+    // borderBottomWidth: Platform.OS === 'ios' ? StyleSheet.hairlineWidth : 0,
+    elevation: 4,
+    // flex: 1,
+    width: 375,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingTop: STATUSBAR_HEIGHT,
+    height: APPBAR_HEIGHT + STATUSBAR_HEIGHT
+  },
+  flex1: {
+    height: 35,
+    flex: 1,
+    paddingLeft: 20
+  },
+  headerBackButton: {
+    
+  },
+  backButtonIcon: {
+    width: 35,
+    height: 35,
+    fontSize: 30
+  }
+
+});
+
+export default LoginHeader;
