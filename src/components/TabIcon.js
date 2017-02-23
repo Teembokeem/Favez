938e477f7f17ae3bf0b6002216016a40cadd4500
Teembokeem <myungtkim@gmail.com>
2017-02-23 15:26:23 -0800

    add switch casing for rendering navabar icons

--- src/components/TabIcon.js
+++ src/components/TabIcon.js
@@ -3,21 +3,57 @@ import React, {
 } from 'react';
 import {
   Text,
+  StyleSheet
 } from 'react-native';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import EIcon from 'react-native-vector-icons/Entypo';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import OIcon from 'react-native-vector-icons/Octicons';
 
 const propTypes = {
   // selected: PropTypes.bool,
   // title: PropTypes.string,
 };
 
+function renderIcon(display) {
+  switch (display) {
+    case 'featured-play-list':
+      return (
+        <MIcon style={styles.Icon} name={display}/>
+      );
+    case 'bell':
+    case 'search':
+      return (
+        <OIcon style={styles.Icon} name={display}/>
+      );
+    case 'ios-heart':
+      return (
+        <Ionicon style={styles.Icon} name={display} />
+      );
+    case 'emoji-happy':
+      return (
+        <EIcon style={styles.Icon} name={display}/>
+      )
+  }
+
+}
+
 const TabIcon = (props) => (
   <Text
     style={{ color: props.selected ? 'red' : 'black' }}
   >
-    {props.title}
+  
+    {renderIcon(props.display)}
   </Text>
 );
 
 TabIcon.propTypes = propTypes;
+
+const styles = StyleSheet.create({
+  Icon: {
+    fontSize: 22
+  }
+})
 
 export default TabIcon;
