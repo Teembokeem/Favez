2dd260b1cc72c78598ee4a7d02f0f9594711088e
Teembokeem <myungtkim@gmail.com>
2017-02-28 17:02:50 -0800

    add feed state to handle ui actions

---
+++ src/modules/feed/FeedState.js
@@ -0,0 +1,29 @@
+import {Map, fromJS} from 'immutable';
+import {loop, Effects} from 'redux-loop';
+import { Actions } from 'react-native-router-flux';
+
+// Initial state
+const initialState = fromJS({
+  header: {
+    more: false
+  }
+});
+
+// Actions
+const HEADER_MORE_VISIBLE = 'FeedState/HEADER_MORE_VISIBLE';
+
+// Action creators
+export function setVisibility() {
+  return {type: HEADER_MORE_VISIBLE};
+}
+
+// Reducer
+export default function FeedStateReducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case HEADER_MORE_VISIBLE:
+      return state
+        .setIn(['header', 'more'], !state.getIn(['header', 'more']));
+    default :
+      return state;
+  }
+}
