7dca7f17d186e99db58d2a566a046933e6a0beb9
Teembokeem <myungtkim@gmail.com>
2017-02-28 17:03:21 -0800

    add binding for feed state modal

--- src/modules/feed/FeedViewContainer.js
+++ src/modules/feed/FeedViewContainer.js
@@ -5,8 +5,11 @@ import FeedView from './FeedView';
 
 export default connect(
   state => {
+    console.log(state)
     return {
       lists: state.getIn(['list', 'all']),
+      headerMore: state.getIn(['feed', 'header', 'more'])
+      // headerContextMenu: state.getIn(['feed', 'header'])
       // cards: state.getIn(['feed', 'cards']).get('data').toJS(),
       // loading: state.getIn(['feed', 'loading'])
     };
