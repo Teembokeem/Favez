0c83a1ac28eff09fe957288b3706dc840c70e168
Teembokeem <myungtkim@gmail.com>
2017-03-03 11:48:46 -0800

    add private, nsfw, options button areas for create list form

--- src/components/create-list/createListForm/createListForm.js
+++ src/components/create-list/createListForm/createListForm.js
@@ -3,6 +3,9 @@ import {
   StyleSheet,
   View,
   Text,
+  TouchableOpacity,
+  Switch,
+  // Dimensions,
   TextInput
 } from 'react-native';
 import {
@@ -12,8 +15,7 @@ import {
 import {Field, reduxForm} from 'redux-form/immutable';
 import OIcon from 'react-native-vector-icons/Octicons';
 import Ionicon from 'react-native-vector-icons/Ionicons';
-
-
+import SLIcon from 'react-native-vector-icons/SimpleLineIcons';
 
 const renderInput = ({input: {onChange, ...restInput}, ...props}) => {
   return <TextInput style={styles.CreateListFormEmailInput} type={props.type} secureTextEntry={props.secureTextEntry} placeholder={props.placeholder} onChangeText={onChange} {...restInput}/>
@@ -26,6 +28,7 @@ const CreateListForm = props => {
   const submit = values => {
     login(values.toJS())
   };
+  let value = false;
 
   return (
     <View style={styles.CreateListFormContainer}>
@@ -49,20 +52,43 @@ const CreateListForm = props => {
       <View style={styles.CreateListFormFieldLocationContainer}>
         <View style={styles.CreateListFormLocationInputContainer}>
           <Text style={styles.CreateListFormLocationLabel}>{'LOCATION'}</Text>
-          <Field name='Location' component={renderInput} type='text' placeholder='Your Location' style={styles.CreateListFormLocationInput} secureTextEntry={true}/>
+          <Field name='Location' component={renderInput} type='text' placeholder='Your Location' style={styles.CreateListFormLocationInput}/>
         </View>
         <View style={styles.CreateListFormLocationIconContainer}>
-          <Ionicon style={styles.CreateListFormLocationIcon} name='md-key'/>
+          <SLIcon style={styles.CreateListFormLocationIcon} name='globe'/>
         </View>
       </View>
-      <View style={styles.LoginButtonContainer}>
-        <Button
-          onPress={handleSubmit(submit)}
-          iconPlacement='left'
-          submitting={submitting}
-          style={styles.CreateListFormButton}>
-          {'Authenticate'}
-        </Button>
+      <View style={styles.CreateListFormFieldPrivateContainer}>
+        <View style={styles.CreateListFormPrivateInputContainer}>
+          <Text style={styles.CreateListFormPrivateLabel}>{'Private'}</Text>
+        </View>
+        <View style={styles.CreateListFormPrivateIconContainer}>
+          <Switch
+            onValueChange={(cool) => {value = !value;}}
+            style={{marginBottom: 10}}
+            value={value}
+          />
+        </View>
+      </View>
+      <View style={styles.CreateListFormFieldPrivateContainer}>
+        <View style={styles.CreateListFormPrivateInputContainer}>
+          <Text style={styles.CreateListFormPrivateLabel}>{'NSFW'}</Text>
+        </View>
+        <View style={styles.CreateListFormPrivateIconContainer}>
+          <Switch
+            onValueChange={(cool) => {value = !value;}}
+            style={{marginBottom: 10}}
+            value={value}
+          />
+        </View>
+      </View>
+      <View style={styles.CreateListFormFieldOptionsContainer}>
+        <View style={styles.CreateListFormOptionsInputContainer}>
+          <Text style={styles.CreateListFormOptionsLabel}>{'More Options'}</Text>
+        </View>
+        <View style={styles.CreateListFormOptionsIconContainer}>
+          <Ionicon style={styles.CreateListFormCollaboratorIcon} name='md-arrow-round-forward'/>
+        </View>
       </View>
     </View>
   );
@@ -114,7 +140,7 @@ const styles = StyleSheet.create({
   CreateListFormFieldCollaboratorContainer: {
     flex: 1,
     maxHeight: 60,
-    padding: 5,
+    // padding: 5,
     paddingBottom: 0,
     flexDirection: 'row',
     borderBottomWidth: 0.5,
@@ -150,22 +176,33 @@ const styles = StyleSheet.create({
   },
   CreateListFormFieldLocationContainer: {
     flex: 1,
-    padding: 5,
+    // padding: 5,
+    paddingTop: 0,
     paddingBottom: 0,
-    marginTop: 5,
+    // marginTop: 5,
     maxHeight: 60,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#d8d8d8',
+
   },
   CreateListFormLocationIconContainer: {
-    width: 50,
-    alignItems: 'center',
-    justifyContent: 'center'
+    // width: 50,
+    paddingRight: 14,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    flex: 1
   },
   CreateListFormLocationIcon: {
-    fontSize: 23,
-    color: '#d8d8d8'
+    fontSize: 22,
+    // paddingLeft: 5,
+    // width: 65,
+    // height: 65,
+    color: '#81dde2'
+
   },
   CreateListFormLocationInputContainer: {
+    marginTop: 5,
     paddingLeft: 12,
     height: 50,
     flex: 1,
@@ -182,16 +219,81 @@ const styles = StyleSheet.create({
     // paddingTop: -10,
     fontFamily: 'Hind-Regular'
   },
-  CreateListFormButtonContainer: {
-    flex: 1
-  },
-  CreateListFormButton: {
-    width: 320,
+  CreateListFormFieldPrivateContainer: {
     flex: 1,
-    borderRadius: 20,
-    alignSelf: 'center'
+    maxHeight: 60,
+    // padding: 5,
+    paddingBottom: 0,
+    flexDirection: 'row',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#d8d8d8',
+    alignItems: 'center'
+  },
+  CreateListFormPrivateIconContainer: {
+    width: 50,
+    paddingRight: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  CreateListFormPrivateIcon: {
+    fontSize: 23,
+    color: '#7f7f7f'
+  },
+  CreateListFormPrivateInputContainer: {
+    flex: 1,
+    height: 50,
+    justifyContent: 'center',
+    paddingLeft: 12
+  },
+  CreateListFormPrivateLabel: {
+    // flex: 1,
+    fontSize: 18,
+    fontFamily: 'Hind-Medium',
+    justifyContent: 'center'
+    // color: '/'
+  },
+  CreateListFormPrivateInput: {
+    flex: 1,
+    paddingTop: -10,
+    fontFamily: 'Hind-Regular'
+  },
+  CreateListFormFieldOptionsContainer: {
+    flex: 1,
+    maxHeight: 60,
+    // padding: 5,
+    paddingBottom: 0,
+    flexDirection: 'row',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#d8d8d8',
+    alignItems: 'center'
+  },
+  CreateListFormOptionsIconContainer: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  CreateListFormOptionsIcon: {
+    fontSize: 23,
+    color: '#7f7f7f'
+  },
+  CreateListFormOptionsInputContainer: {
+    flex: 1,
+    height: 50,
+    justifyContent: 'center',
+    paddingLeft: 12
+  },
+  CreateListFormOptionsLabel: {
+    // flex: 1,
+    fontSize: 18,
+    fontFamily: 'Hind-Medium',
+    justifyContent: 'center'
+    // color: '/'
+  },
+  CreateListFormOptionsInput: {
+    flex: 1,
+    paddingTop: -10,
+    fontFamily: 'Hind-Regular'
   }
-
 });
 
 export default reduxForm({
