37c24138e8f2a3182e872ff795a7560aba015241
Teembokeem <myungtkim@gmail.com>
2017-03-07 13:28:42 -0800

    add reducer switch casing for fave scrape request. add error props on the state for fave for ease of debugging.

--- src/redux/fave/faveReducer.js
+++ src/redux/fave/faveReducer.js
@@ -1,14 +1,19 @@
 import {fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
 import {
-  SET_NEWFAVE
+  SET_NEWFAVE,
+  FAVE_SCRAPE_REQUEST,
+  FAVE_SCRAPE_SUCCESS,
+  FAVE_SCRAPE_FAILURE,
+  requestScrape
 } from './faveActions';
 
 // Initial state
 const initialState = fromJS({
   all: [],
   current: {},
-  loading: true
+  loading: true,
+  error: {}
 });
 
 // Reducer
@@ -17,6 +22,18 @@ export default function FaveReducer(state = initialState, action = {}) {
     case SET_NEWFAVE:
       return state
         .set('current', action.payload);
+    case FAVE_SCRAPE_REQUEST:
+      return loop(
+        state.set('loading', true),
+        Effects.promise(() => requestScrape(action.payload))
+      );
+    case FAVE_SCRAPE_SUCCESS:
+      return state
+        .set('loading', false)
+        .set('all', action.payload.data);
+    case FAVE_SCRAPE_FAILURE:
+      console.log('ERROR');
+      return state.set('error', action.payload);
     default:
       return state;
   }
