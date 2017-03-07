3174bc34e2926cd7bf4282132d9b7811c657d80c
Teembokeem <myungtkim@gmail.com>
2017-03-07 15:07:28 -0800

    add ui service for api calls

---
+++ src/services/ui.js
@@ -0,0 +1,29 @@
+import {get, post, put, delete} from '../utils/api';
+
+export async function getFavezAll() {
+  return get('/favez/all');
+}
+
+export async function uiScrapeUrl(data) {
+  // let body = {url: data};
+  // return await post('/favez/scrape', body);
+  
+  console.log('IN UI SERVICE...SENDING FAKE DATA FOR NOW....');
+  return new Promise((resolve, reject) => {
+    const sendTempArr = true;
+    const testTitle = 'Faked Imgur Scrape';
+    const TempArr = [
+      'https://i.imgur.com/OIW3k7Kb.jpg',
+      'https://i.imgur.com/vDhHXkjb.jpg',
+      'https://i.imgur.com/hUR0JhYb.jpg',
+      'https://i.imgur.com/jrOGcatb.jpg',
+      'https://i.imgur.com/Oj1hIUab.jpg',
+      'https://i.imgur.com/QReYxk2b.jpg'
+    ];
+    if (sendTempArr) {
+      resolve({status: 200, data: TempArr, url: data, title: testTitle});
+    } else {
+      reject({status: 404, err: 'you messed up man...'});
+    }
+  });
+}
