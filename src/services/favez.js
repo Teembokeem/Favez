14dc4e9f056b6cc3757ef2a8e28a00a7b066ac9f
Teembokeem <myungtkim@gmail.com>
2017-03-07 15:07:41 -0800

    modify fave service for scrape

--- src/services/favez.js
+++ src/services/favez.js
@@ -3,26 +3,3 @@ import {get, post, put, delete} from '../utils/api';
 export async function getFavezAll() {
   return get('/favez/all');
 }
-
-export async function faveScrapeUrl(data) {
-  // let body = {url: data};
-  // return await post('/favez/scrape', body);
-  
-  console.log('IN FAVE SERVICE...SENDING FAKE DATA FOR NOW....');
-  return new Promise((resolve, reject) => {
-    const sendTempArr = true;
-    const TempArr = [
-      'https://i.imgur.com/OIW3k7Kb.jpg',
-      'https://i.imgur.com/vDhHXkjb.jpg',
-      'https://i.imgur.com/hUR0JhYb.jpg',
-      'https://i.imgur.com/jrOGcatb.jpg',
-      'https://i.imgur.com/Oj1hIUab.jpg',
-      'https://i.imgur.com/QReYxk2b.jpg'
-    ];
-    if (sendTempArr) {
-      resolve({status: 200, data: TempArr, url: data});
-    } else {
-      reject({status: 404, err: 'you messed up man...'});
-    }
-  });
-}
