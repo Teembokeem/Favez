bf733e2056f07277e21f156f60181df8afb304ce
Teembokeem <myungtkim@gmail.com>
2017-03-07 15:07:18 -0800

    create ui actions file for future ui interactions

---
+++ src/redux/ui/uiActions.js
@@ -0,0 +1,29 @@
+import {Actions} from 'react-native-router-flux';
+import {
+  uiScrapeUrl
+} from '../../services/ui';
+
+// Actions
+export const UI_BROWSER_SET_URL = 'UI_BROWSER_SET_URL';
+export const UI_BROWSER_SCRAPE_REQUEST = 'UI_BROWSER_SCRAPE_REQUEST';
+export const UI_BROWSER_SCRAPE_SUCESS = 'UI_BROWSER_SCRAPE_SUCESS';
+export const UI_BROWSER_SCRAPE_FAILURE = 'UI_BROWSER_SCRAPE_FAILURE';
+
+// Action creators
+export function setBrowserUrl(url) {
+  console.log('NEVNEIVNESILVNEILSNVLIESNVIELSNVIELSNVEILSVNEILSVNSEILVENSILVESNVILSENVILSVNSILVENSILVESILVSENVLEISNVEILSNVSEILVNSELI')
+  return {type: UI_BROWSER_SET_URL, payload: url};
+}
+
+// SCRAPE URL
+export async function scrapeUrl(url) {
+  console.log('Fave Model Action: scrapeUrl');
+  console.log('returning action type');
+  return {type: UI_BROWSER_SCRAPE_REQUEST, payload: url};
+}
+
+export async function requestScrape(data) {
+  return await uiScrapeUrl(data)
+    .then((res) => ({type: UI_BROWSER_SCRAPE_SUCESS, payload: res}))
+    .catch((err) => ({type: UI_BROWSER_SCRAPE_FAILURE, payload: err}));
+}
