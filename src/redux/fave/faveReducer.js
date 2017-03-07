351b17bbc1551608a7951835b00bb1ad9d3ebb14
Teembokeem <myungtkim@gmail.com>
2017-03-07 15:07:10 -0800

    remove fave scrape actions to ui reducer

--- src/redux/fave/faveReducer.js
+++ src/redux/fave/faveReducer.js
@@ -1,11 +1,7 @@
 import {fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
 import {
-  SET_NEWFAVE,
-  FAVE_SCRAPE_REQUEST,
-  FAVE_SCRAPE_SUCCESS,
-  FAVE_SCRAPE_FAILURE,
-  requestScrape
+  SET_NEWFAVE
 } from './faveActions';
 
 // Initial state
@@ -22,18 +18,6 @@ export default function FaveReducer(state = initialState, action = {}) {
     case SET_NEWFAVE:
       return state
         .set('current', action.payload);
-    case FAVE_SCRAPE_REQUEST:
-      return loop(
-        state.set('loading', true),
-        Effects.promise(() => requestScrape(action.payload))
-      );
-    case FAVE_SCRAPE_SUCCESS:
-      return state
-        .set('loading', false)
-        .set('current', {url: action.payload.url, scrapedImages: action.payload.data});
-    case FAVE_SCRAPE_FAILURE:
-      console.log('ERROR');
-      return state.set('error', action.payload);
     default:
       return state;
   }
