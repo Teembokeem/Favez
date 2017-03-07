d98dcdd2c055fff8be40bd75d16c17778b542f80
Teembokeem <myungtkim@gmail.com>
2017-03-07 13:47:39 -0800

    clean up fave actions, set separators for coupled export functions

--- src/redux/fave/faveActions.js
+++ src/redux/fave/faveActions.js
@@ -1,13 +1,10 @@
 import {Actions} from 'react-native-router-flux';
-// import {getListAll} from '../../services/list';
 import {
   faveScrapeUrl
-} from '../../services/fave';
+} from '../../services/favez';
+
+
 // Actions
-// export const INCREMENT = 'INCREMENT';
-// export const LIST_REQUEST = 'LIST_REQUEST';
-// export const LIST_RESPONSE = 'LIST_RESPONSE';
-// export const LIST_LIST = 'LIST_LIST';
 export const SET_NEWFAVE = 'SET_NEWFAVE';
 export const FAVE_SCRAPE_REQUEST = 'FAVE_SCRAPE_REQUEST';
 export const FAVE_SCRAPE_SUCCESS = 'FAVE_SCRAPE_SUCCESS';
@@ -22,6 +19,7 @@ export async function setFave(fave) {
   };
 }
 
+// SCRAPE URL
 export async function scrapeUrl(url) {
   console.log('Fave Model Action: scrapeUrl');
   console.log('returning action type');
@@ -33,3 +31,5 @@ export async function requestScrape(data) {
     .then((res) => ({type: FAVE_SCRAPE_SUCCESS, payload: res}))
     .catch((err) => ({type: FAVE_SCRAPE_FAILURE, payload: err}));
 }
+
+//
