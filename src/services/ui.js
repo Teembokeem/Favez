9860ccd9ab6a4a0ec21eef02cb3e1a921f598be2
Teembokeem <myungtkim@gmail.com>
2017-03-09 15:36:21 -0800

    uses api endpoint for scraping

--- src/services/ui.js
+++ src/services/ui.js
@@ -5,25 +5,26 @@ export async function getFavezAll() {
 }
 
 export async function uiScrapeUrl(data) {
-  // let body = {url: data};
-  // return await post('/favez/scrape', body);
+  let body = {url: data};
+  return await post('/favez/scrape', body);
   
-  console.log('IN UI SERVICE...SENDING FAKE DATA FOR NOW....');
-  return new Promise((resolve, reject) => {
-    const sendTempArr = true;
-    const testTitle = 'Faked Imgur Scrape';
-    const TempArr = [
-      'https://i.imgur.com/OIW3k7Kb.jpg',
-      'https://i.imgur.com/vDhHXkjb.jpg',
-      'https://i.imgur.com/hUR0JhYb.jpg',
-      'https://i.imgur.com/jrOGcatb.jpg',
-      'https://i.imgur.com/Oj1hIUab.jpg',
-      'https://i.imgur.com/QReYxk2b.jpg'
-    ];
-    if (sendTempArr) {
-      resolve({status: 200, data: TempArr, url: data, title: testTitle});
-    } else {
-      reject({status: 404, err: 'you messed up man...'});
-    }
-  });
+  // console.log('IN UI SERVICE...SENDING FAKE DATA FOR NOW....');
+  // return new Promise((resolve, reject) => {
+  //   const sendTempArr = true;
+  //   const testTitle = 'Faked Imgur Scrape';
+  //   const TempArr = [
+  //     'https://i.imgur.com/OIW3k7Kb.jpg',
+  //     'https://i.imgur.com/vDhHXkjb.jpg',
+  //     'https://i.imgur.com/hUR0JhYb.jpg',
+  //     'https://i.imgur.com/jrOGcatb.jpg',
+  //     'https://i.imgur.com/Oj1hIUab.jpg',
+  //     'https://i.imgur.com/QReYxk2b.jpg'
+  //   ];
+  //   if (sendTempArr) {
+  //     console.log('sending arrays', TempArr)
+  //     resolve({status: 200, data: TempArr, url: data, title: testTitle});
+  //   } else {
+  //     reject({status: 404, err: 'you messed up man...'});
+  //   }
+  // });
 }
