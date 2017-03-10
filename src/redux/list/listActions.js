c04421dedf39639d516da6fd030e5feafcea946e
Teembokeem <myungtkim@gmail.com>
2017-03-10 12:08:44 -0800

    add create list method on the list actions. awaiting list service api call, and reducer handling

--- src/redux/list/listActions.js
+++ src/redux/list/listActions.js
@@ -1,5 +1,8 @@
 import {Actions} from 'react-native-router-flux';
-import {getListAll} from '../../services/list';
+import {
+  getListAll,
+  listCreate
+} from '../../services/list';
 
 // Actions
 export const INCREMENT = 'INCREMENT';
@@ -7,6 +10,9 @@ export const LIST_REQUEST = 'LIST_REQUEST';
 export const LIST_RESPONSE = 'LIST_RESPONSE';
 export const LIST_LIST = 'LIST_LIST';
 export const SET_LIST = 'SET_LIST';
+export const LIST_CREATE_REQUEST = 'LIST_CREATE_REQUEST';
+export const LIST_CREATE_SUCCESS = 'LIST_CREATE_SUCCESS';
+export const LIST_CREATE_FAILURE = 'LIST_CREATE_FAILURE';
 
 // Action creators
 export function increment(cards, index) {
@@ -21,17 +27,30 @@ export async function getFullList() {
   };
 }
 
+export async function requestFullList() {
+  return {
+    type: LIST_RESPONSE,
+    payload: await getListAll()
+  };
+}
+
 export async function setList(index) {
-  console.log('this index now', index)
+  console.log('this index now', index);
   return {
     type: SET_LIST,
     payload: index
   };
 }
 
-export async function requestFullList() {
+export async function createList(obj) {
   return {
-    type: LIST_RESPONSE,
-    payload: await getListAll()
+    type: LIST_CREATE_REQUEST,
+    payload: obj
   };
+}
+
+export async function requestCreateList(data) {
+  return await listCreate(data)
+    .then((res) => ({type: LIST_CREATE_SUCCESS, payload: res}))
+    .then((err) => ({type: LIST_CREATE_FAILURE, payload: err}));
 }
