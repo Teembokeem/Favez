9b7b129edef88fd89362f252ff2e8c78457e2c38
Teembokeem <myungtkim@gmail.com>
2017-03-10 13:32:47 -0800

    add boiler for add fave form view

---
+++ src/modules/add-fave-form/AddFaveFormView.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import * as UIActions from '../../redux/ui/uiActions';
+import {
+  View,
+  ScrollView,
+  StyleSheet
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import Header from '../../components/globals/header/header';
+const AddFaveFormView = React.createClass({
+  propTypes: {},
+
+  componentWillMount() {
+    // this.props.dispatch(ListActions.getFullList());
+  },
+
+
+  render() {
+    const {index, lists, headerMore, browser} = this.props;
+    console.log('INSTANTIATING ADD FAVE VIEW', this.props)
+    return (
+      <View style={styles.container}>
+        {/*<AddFaveFormHeader />*/}
+        <ScrollView>
+          <Header title={'Add Fave'}/>
+          <View>
+            <View>
+              <Image></Image>
+            </View>
+            <View>
+              <Text>{'Site Text'}</Text>
+              <Text></Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    // flexGrow: 1,
+    flex: 1,
+    backgroundColor: 'white',
+    // justifyContent: 'center',
+    // height: 1000,
+    paddingTop: 20,
+    paddingBottom: 50,
+    alignItems: 'center'
+    // justifyContent: 'center'
+  },
+});
+
+export default AddFaveFormView;
