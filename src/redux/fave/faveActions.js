c092fa43311d2ace5b4f015aa71668e905c6ab35
Teembokeem <myungtkim@gmail.com>
2017-03-10 13:49:51 -0800

    create separate function for set new faves in case theres a divergence

--- src/redux/fave/faveActions.js
+++ src/redux/fave/faveActions.js
@@ -1,12 +1,20 @@
 import {Actions} from 'react-native-router-flux';
 
 // Actions
-export const SET_NEWFAVE = 'SET_NEWFAVE';
+export const FAVE_SET_NEWFAVE = 'FAVE_SET_NEWFAVE';
 // Action creators
 export async function setFave(fave) {
-  console.log('this fave now', fave)
+  console.log('this fave now', fave);
   return {
-    type: SET_NEWFAVE,
+    type: FAVE_SET_NEWFAVE,
+    payload: fave
+  };
+}
+
+export async function setNewFave(fave) {
+  console.log('this fave now', fave);
+  return {
+    type: FAVE_SET_NEWFAVE,
     payload: fave
   };
 }
