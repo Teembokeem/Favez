5d55f56e3d24426168585cdb517b3b4d528e10cc
Teembokeem <myungtkim@gmail.com>
2017-03-10 16:14:26 -0800

    add syntax correction for list to state props

--- src/modules/add-fave-form/AddFaveFormViewContainer.js
+++ src/modules/add-fave-form/AddFaveFormViewContainer.js
@@ -6,7 +6,7 @@ import AddFaveFormView from './AddFaveFormView';
 export default connect(
   state => {
     return {
-      lists: state.getIn(['list', 'myLists']),
+      lists: state.getIn(['list', 'myLists']).toJS(),
       fave: state.getIn(['fave', 'current'])
       // headerContextMenu: state.getIn(['feed', 'header'])
       // cards: state.getIn(['feed', 'cards']).get('data').toJS(),
