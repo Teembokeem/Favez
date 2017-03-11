b09abac4f555b74c0ba5ee810bf6fe0c1316fe8d
Teembokeem <myungtkim@gmail.com>
2017-03-10 16:14:14 -0800

    set renderList Method for retrieving lists

--- src/modules/add-fave-form/AddFaveFormView.js
+++ src/modules/add-fave-form/AddFaveFormView.js
@@ -3,15 +3,27 @@ import * as UIActions from '../../redux/ui/uiActions';
 import {
   View,
   ScrollView,
+  Image,
+  Text,
   StyleSheet
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import Header from '../../components/globals/header/header';
+import AddFaveFormHeader from '../../components/add-fave-form/addFaveFormHeader/addFaveFormHeader';
+
 const AddFaveFormView = React.createClass({
   propTypes: {},
 
   componentWillMount() {
     // this.props.dispatch(ListActions.getFullList());
+  },
+
+  renderList(lists) {
+    lists.forEach((list) => {
+      return (
+        <Text>{list.name}</Text>
+      );
+    });
   },
 
 
@@ -20,17 +32,25 @@ const AddFaveFormView = React.createClass({
     console.log('INSTANTIATING ADD FAVE VIEW', this.props)
     return (
       <View style={styles.container}>
-        {/*<AddFaveFormHeader />*/}
+        <AddFaveFormHeader />
         <ScrollView>
           <Header title={'Add Fave'}/>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{padding: 10}}>
+              <Image style={{height: 55, width: 55, borderRadius: 10}} source={{uri: fave.image}}/>
+            </View>
+            <View style={{paddingTop: 10}}>
+              <Text style={{fontFamily: 'Hind-Bold', fontSize: 18}}>{'Site Text'}</Text>
+              <Text style={{fontFamily: 'Hind-Medium', fontSize: 13}}>{fave.image}</Text>
+            </View>
+          </View>
           <View>
-            <View>
-              <Image></Image>
-            </View>
-            <View>
-              <Text>{'Site Text'}</Text>
-              <Text></Text>
-            </View>
+            <Text style={{fontSize: 25, fontFamily: 'Hind-Bold', paddingLeft: 10, paddingTop: 10}}>LISTS</Text>
+            {lists.map((list, idx) => {
+              return (
+                <Text style={{paddingLeft: 10, fontFamily: 'Hind-Medium', fontSize: 18}} key={'list ' + idx}>{list.name}</Text>
+              )
+            })}
           </View>
         </ScrollView>
       </View>
