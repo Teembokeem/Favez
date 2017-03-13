db84e7404373be026c1ffd7febe6c0c826853b30
Teembokeem <myungtkim@gmail.com>
2017-03-13 14:32:48 -0700

    remove logs on the auth api js

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -64,7 +64,6 @@ export async function del(path, suppressRedBox) {
 export async function request(method, path, body, suppressRedBox) {
   try {
     const response = await sendRequest(method, path, body, suppressRedBox);
-    console.log('response', response)
     return handleResponse(
       path,
       response
