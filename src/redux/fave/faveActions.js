bdb0287563af7cd9c0f4e90c9256cceeca675fe7
Teembokeem <myungtkim@gmail.com>
2017-03-14 14:07:39 -0700

    add create fave action, and request handler in fave actions

--- src/redux/fave/faveActions.js
+++ src/redux/fave/faveActions.js
@@ -1,7 +1,14 @@
 import {Actions} from 'react-native-router-flux';
+import {
+  favezCreateFave
+} from '../../services/favez';
 
 // Actions
 export const FAVE_SET_NEWFAVE = 'FAVE_SET_NEWFAVE';
+export const FAVE_CREATE_REQUEST = 'FAVE_CREATE_REQUEST';
+export const FAVE_CREATE_SUCCESS = 'FAVE_CREATE_SUCCESS';
+export const FAVE_CREATE_FAILURE = 'FAVE_CREATE_FAILURE';
+
 // Action creators
 export async function setFave(fave) {
   console.log('this fave now', fave);
@@ -18,6 +25,20 @@ export async function setNewFave(fave) {
     payload: fave
   };
 }
+
+export async function createFave(fave) {
+  return {
+    type: FAVE_CREATE_REQUEST,
+    payload: fave
+  };
+}
+
+export async function requestCreateFave(data) {
+  return await favezCreateFave(data)
+    .then((res) => ({type: FAVE_CREATE_SUCCESS, payload: res}))
+    .catch((err) => ({type: FAVE_CREATE_FAILURE, payload: err}));
+}
+
 
 //
 
