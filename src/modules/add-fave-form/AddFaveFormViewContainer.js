d373e662211268a84ff434468bb483d31786f96e
Teembokeem <myungtkim@gmail.com>
2017-03-14 12:13:48 -0700

    add data bindings to add fave form container

--- src/modules/add-fave-form/AddFaveFormViewContainer.js
+++ src/modules/add-fave-form/AddFaveFormViewContainer.js
@@ -6,8 +6,12 @@ import AddFaveFormView from './AddFaveFormView';
 export default connect(
   state => {
     return {
-      lists: state.getIn(['list', 'myLists']).toJS(),
-      fave: state.getIn(['fave', 'current'])
+      myLists: state.getIn(['list', 'myLists']).toJS(),
+      myCollabs: state.getIn(['list', 'collaborations']) ? state.getIn(['list', 'collaborations']).toJS() : [],
+      fave: state.getIn(['fave', 'current']),
+      tabs: state.getIn(['ui','addFaveForm', 'tabs', 'set']),
+      selectedTab: state.getIn(['ui', 'addFaveForm', 'tabs', 'selected']),
+      selectedRadio: state.getIn(['ui', 'addFaveForm', 'radio'])
       // headerContextMenu: state.getIn(['feed', 'header'])
       // cards: state.getIn(['feed', 'cards']).get('data').toJS(),
       // loading: state.getIn(['feed', 'loading'])
