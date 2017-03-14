cc2a16eea3e741d40829b5d2de52a3895357a482
Teembokeem <myungtkim@gmail.com>
2017-03-14 12:12:10 -0700

    add add fave form submit button

---
+++ src/components/add-fave-form/addFaveFormSubmit/addFaveFormSubmit.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Actions} from 'react-native-router-flux';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+const window = Dimensions.get('window');
+
+const addFaveFormSubmit = React.createClass({
+
+  render() {
+    console.log(this.props);
+    return (
+    <View style={styles.addFaveFormSubmitContainer}>
+      <TextInput
+        style={styles.TextInput}
+        placeholder={'Type a message (optional)'}
+        placeholderTextColor={'#c8c8c8'}
+      />
+      <TouchableOpacity
+      style={styles.FormSubmitButton}
+      >
+        <Text
+        style={styles.FormSubmitButtonText}
+        >ADD TO FAVEZ</Text>
+      </TouchableOpacity>
+    </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  addFaveFormSubmitContainer: {
+    borderTopWidth: 0.5,
+    borderTopColor: '#d8d8d8',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    // position: 'absolute',
+    // bottom: 0,
+    flexDirection: 'column',
+    width: window.width
+  },
+  TextInput: {
+    flex: 1,
+    height: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontFamily: 'Hind-Regular',
+    fontSize: 13,
+    color: 'black'
+  },
+  FormSubmitButton: {
+    flex: 1,
+    width: window.width,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4caf4e'
+  },
+  FormSubmitButtonText: {
+    color: 'white',
+    fontFamily: 'Hind-Bold'
+  }
+
+});
+
+export default addFaveFormSubmit;
