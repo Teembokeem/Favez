d0a6002018dd70d9973d76ba37976235aaeb5ee1
Teembokeem <myungtkim@gmail.com>
2017-03-14 14:06:36 -0700

    clean up add fave browser

--- src/modules/add-fave-browse/AddFaveBrowseView.js
+++ src/modules/add-fave-browse/AddFaveBrowseView.js
@@ -34,10 +34,10 @@ const AddFaveBrowseView = React.createClass({
   },
 
   render() {
-    const {index, lists, headerMore, browser} = this.props;
-    console.log('INSTANTIATING ADD FAVE VIEW', this.props)
+    const {browser} = this.props;
+    console.log('INSTANTIATING ADD FAVE VIEW', this.props);
     return (
-      <View style={{flex: 1}}>
+      <View style={styles.container}>
         <FavezBrowser
           browser={browser}
           scrapeUrl={this.scrapeUrl}
@@ -51,15 +51,8 @@ const AddFaveBrowseView = React.createClass({
 
 const styles = StyleSheet.create({
   container: {
-    // flexGrow: 1,
-    backgroundColor: '#e9e9e9',
-    // justifyContent: 'center',
-    // height: 1000,
-    paddingTop: 20,
-    paddingBottom: 50,
-    alignItems: 'center'
-    // justifyContent: 'center'
-  },
+    flex: 1
+  }
 });
 
 export default AddFaveBrowseView;
