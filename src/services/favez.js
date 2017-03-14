28e521c1530a2e4f61391c016299fce2cc5e558a
Teembokeem <myungtkim@gmail.com>
2017-03-14 14:07:50 -0700

    add service method for create fave in fave service

--- src/services/favez.js
+++ src/services/favez.js
@@ -3,3 +3,7 @@ import {get, post, put, delete} from '../utils/api';
 export async function getFavezAll() {
   return get('/favez/all');
 }
+
+export async function favezCreateFave() {
+  return post('/favez');
+}
