57d50b68e6eec9d7938afb08f4c1ba75eece73c0
Teembokeem <myungtkim@gmail.com>
2017-03-14 15:50:46 -0700

    add case for fave create request

--- src/redux/fave/faveReducer.js
+++ src/redux/fave/faveReducer.js
@@ -1,7 +1,11 @@
 import {fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
 import {
-  FAVE_SET_NEWFAVE
+  FAVE_SET_NEWFAVE,
+  FAVE_CREATE_REQUEST,
+  FAVE_CREATE_SUCCESS,
+  FAVE_CREATE_FAILURE,
+  requestCreateFave
 } from './faveActions';
 
 // Initial state
@@ -18,6 +22,20 @@ export default function FaveReducer(state = initialState, action = {}) {
     case FAVE_SET_NEWFAVE:
       return state
         .set('current', action.payload);
+    case FAVE_CREATE_REQUEST:
+      console.log('FAVE CREATE REQUEST');
+      return loop(
+        state.set('loading', true),
+        Effects.promise(() => requestCreateFave(action.payload))
+      );
+    case FAVE_CREATE_SUCCESS:
+      console.log('SUCCESS', action);
+      return state
+        .set('loading', false)
+        .set('current', {});
+    case FAVE_CREATE_FAILURE:
+      console.log('ERROR', action);
+      return state.set('ERROR', action);
     default:
       return state;
   }
