03906ce13637ed48a5aae1263446b9140a670ece
Teembokeem <myungtkim@gmail.com>
2017-03-15 17:05:55 -0700

    flush out render list method for favorite view

--- src/modules/favorite/FavoriteView.js
+++ src/modules/favorite/FavoriteView.js
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-import * as FavoriteState from './FavoriteState';
 import {
   Text,
   View,
@@ -7,7 +6,7 @@ import {
   StyleSheet
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
-
+import * as UIActions from '../../redux/ui/uiActions';
 import FavoriteHeader from '../../components/favorite/favoriteHeader/favoriteHeader';
 import Header from '../../components/globals/header/header';
 import HeaderTabs from '../../components/globals/headerTabs/headerTabs';
@@ -16,45 +15,59 @@ import Card from '../../components/globals/card/card';
 
 const FavoriteView = React.createClass({
   propTypes: {},
-  renderChildren() {
-    switch (this.props.selected) {
+  renderList() {
+    const {myLists, myCollabs, selectedTab, user} = this.props;
+    switch (selectedTab) {
       case 'your lists':
+      console.log('1', myLists)
+        return renderListChildren(myLists, 'list');
       case 'collabs':
-        console.log(this.props.selected)
-        return (
-          this.props.lists.map((list, index) => (
-              <List
-                list={list}
-                key={'list ' + index}
-              >
-              </List>
-          ))
-        );
+      console.log('2')
+        return renderListChildren(myCollabs, 'list');
       case 'liked':
-        return (
-          this.props.favez.map((fave, index) => (
-              <Card
-                key={'fave ' + index}
-                card={fave}
-                track={index}
-                moving={this.moving}
-                increment={this.increment}
-            />
-          ))
-        );
+      console.log('3')
+        return renderListChildren(myLists, 'card');
       default :
         return null;
     }
-  },
+    function renderListChildren(listSet, childType) {
+      switch (childType) {
+        case 'list':
+        console.log('hello')
+          return (
+            listSet.map((list, index) => (
+                <List
+                  list={list}
+                  user={user}
+                  key={'list ' + index}
+                >
+                </List>
+            ))
+          );
+        case 'card':
+        default :
+          return (
+            this.props.favez.map((fave, index) => (
+                <Card
+                  key={'fave ' + index}
+                  card={fave}
+                  track={index}
+                  moving={this.moving}
+              />
+            ))
+          );
+      }
 
-  setFilter(val) {
-    this.props.dispatch(FavoriteState.setFilter(val));
+    }
+  },
+  setFilter(view, tab) {
+    this.props.dispatch(UIActions.setViewTab(view, tab));
   },
 
   render() {
-    const child = this.renderChildren();
-    // const {faves, lists} = this.props;
-    const selectedTab = this.props.selected;
+    const {tabs, selectedTab} = this.props;
+    const child = this.renderList();
+    console.log('this props', this.props)
     return (
       <View style={styles.container}>
         <ScrollView
@@ -62,9 +75,10 @@ const FavoriteView = React.createClass({
           <FavoriteHeader />
           <Header title={'FAVEZ'} />
           <HeaderTabs
+            view={'favorite'}
             setFilter={this.setFilter}
             selected={selectedTab}
-            tabs={['your lists', 'collabs', 'liked']}
+            tabs={tabs}
           />
           <View
             style={styles.contentContainer}
