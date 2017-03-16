e12db3056cdf146fd84799c979b017e073623a95
Teembokeem <myungtkim@gmail.com>
2017-03-15 17:06:06 -0700

    set proper props paths for favorite view container

--- src/modules/favorite/FavoriteViewContainer.js
+++ src/modules/favorite/FavoriteViewContainer.js
@@ -1,20 +1,18 @@
-import {connect} from 'react-redux';
+import {
+  connect
+} from 'react-redux';
 import FavoriteView from './FavoriteView';
+
 export default connect(
   state => {
-    const filteredLists = state.getIn(['favorite', 'lists']).get('data').toJS().filter((list) => {
-      if (state.getIn(['favorite', 'selected']) === 'your lists') {
-        return state.getIn(['favorite', 'user', 'data']).toJS().username === list.creator.name;
-      } else {
-        return state.getIn(['favorite', 'user', 'data']).toJS().username !== list.creator.name;
-      }
-    });
     return {
-      lists: filteredLists,
-      user: state.getIn(['profile', 'user', 'data']).toJS(),
-      favez: state.getIn(['favorite', 'favez']).get('data').toJS(),
-      selected: state.getIn(['favorite', 'selected']),
-      loading: state.getIn(['search', 'loading'])
+      myLists: state.getIn(['list', 'myLists']),
+      myCollabs: state.getIn(['list', 'collaborations']),
+      tabs: state.getIn(['ui','favorite', 'tabs', 'set']).toJS(),
+      selectedTab: state.getIn(['ui', 'favorite', 'tabs', 'selected']),
+      user: state.getIn(['user', 'user']),
+      // favez: state.getIn(['favorite', 'favez']).get('data').toJS(),
+      // loading: state.getIn(['search', 'loading'])
     };
   }
 )(FavoriteView);
