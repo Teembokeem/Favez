9a28e8607b090a4970083562a9414d5d5bfd20a7
Teembokeem <myungtkim@gmail.com>
2017-03-17 17:20:04 -0700

    add header context menu to state props on favorite view fontainer

--- src/modules/favorite/FavoriteViewContainer.js
+++ src/modules/favorite/FavoriteViewContainer.js
@@ -11,6 +11,7 @@ export default connect(
       tabs: state.getIn(['ui','favorite', 'tabs', 'set']).toJS(),
       selectedTab: state.getIn(['ui', 'favorite', 'tabs', 'selected']),
       user: state.getIn(['user', 'user']),
+      headerContextMenu: state.getIn(['ui', 'favorite', 'header', 'contextMenu']).toJS()
       // favez: state.getIn(['favorite', 'favez']).get('data').toJS(),
       // loading: state.getIn(['search', 'loading'])
     };
