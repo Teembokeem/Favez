f33cfd5bc42419b60538211582ff53369e4d556c
Teembokeem <myungtkim@gmail.com>
2017-03-17 17:20:30 -0700

    add state props for context menu on feed view state

--- src/modules/feed/FeedViewContainer.js
+++ src/modules/feed/FeedViewContainer.js
@@ -8,7 +8,7 @@ export default connect(
     console.log(state)
     return {
       lists: state.getIn(['list', 'all']),
-      headerMore: state.getIn(['feed', 'header', 'more'])
+      headerContextMenu: state.getIn(['ui', 'feed', 'header', 'contextMenu']).toJS()
       // headerContextMenu: state.getIn(['feed', 'header'])
       // cards: state.getIn(['feed', 'cards']).get('data').toJS(),
       // loading: state.getIn(['feed', 'loading'])
