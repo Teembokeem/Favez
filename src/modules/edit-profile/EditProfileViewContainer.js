cf2a9ed0a1b062f5fd8e29502fe4d8abc013dfcd
Teembokeem <myungtkim@gmail.com>
2017-03-22 12:20:10 -0700

    add boiler edit profile view container

---
+++ src/modules/edit-profile/EditProfileViewContainer.js
@@ -0,0 +1,20 @@
+import {
+  connect
+} from 'react-redux';
+import FavoriteView from './EditProfileView';
+
+export default connect(
+  state => {
+    return {
+      myLists: state.getIn(['list', 'myLists']),
+      myCollabs: state.getIn(['list', 'collaborations']),
+      tabs: state.getIn(['ui','favorite', 'tabs', 'set']).toJS(),
+      selectedTab: state.getIn(['ui', 'favorite', 'tabs', 'selected']),
+      user: state.getIn(['user', 'user']),
+      headerContextMenu: state.getIn(['ui', 'favorite', 'header', 'contextMenu']).toJS(),
+      listContextMenu: state.getIn(['ui', 'favorite', 'list', 'contextMenu']).toJS()
+      // favez: state.getIn(['favorite', 'favez']).get('data').toJS(),
+      // loading: state.getIn(['search', 'loading'])
+    };
+  }
+)(FavoriteView);
