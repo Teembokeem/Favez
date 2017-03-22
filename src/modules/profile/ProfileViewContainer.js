05e4e576448673989053af418e36210cc8490257
Teembokeem <myungtkim@gmail.com>
2017-03-22 09:55:35 -0700

    add correct binding for state

--- src/modules/profile/ProfileViewContainer.js
+++ src/modules/profile/ProfileViewContainer.js
@@ -17,22 +17,23 @@ function grabComments(lists) {
 
 export default connect(
   state => {
-    const filteredLists = state.getIn(['profile', 'lists']).get('data').toJS().filter((list) => {
-      if (state.getIn(['profile', 'selected']) === 'lists') {
-        return state.getIn(['profile', 'user', 'data']).toJS().username === list.creator.name;
-      } else if (state.getIn(['profile', 'selected']) === 'collabs') {
-        return state.getIn(['profile', 'user', 'data']).toJS().username !== list.creator.name && list.collaborators.indexOf(state.getIn(['profile', 'user', 'data']).toJS().username) > -1;
-      } else {
-        return state.getIn(['profile', 'user', 'data']).toJS().username !== list.creator.name && list.collaborators.indexOf(state.getIn(['profile', 'user', 'data']).toJS().username) === -1;
-      }
-    });
+    // const filteredLists = state.getIn(['profile', 'lists']).get('data').toJS().filter((list) => {
+    //   if (state.getIn(['profile', 'selected']) === 'lists') {
+    //     return state.getIn(['profile', 'user', 'data']).toJS().username === list.creator.name;
+    //   } else if (state.getIn(['profile', 'selected']) === 'collabs') {
+    //     return state.getIn(['profile', 'user', 'data']).toJS().username !== list.creator.name && list.collaborators.indexOf(state.getIn(['profile', 'user', 'data']).toJS().username) > -1;
+    //   } else {
+    //     return state.getIn(['profile', 'user', 'data']).toJS().username !== list.creator.name && list.collaborators.indexOf(state.getIn(['profile', 'user', 'data']).toJS().username) === -1;
+    //   }
+    // });
+    console.log(state.getIn(['user', 'user']))
     return {
-      user: state.getIn(['profile', 'user', 'data']).toJS(),
-      lists: filteredLists,
+      user: state.getIn(['user', 'user']),
+      lists: state.getIn(['list', 'myLists']),
       comments: grabComments(state.getIn(['profile', 'lists']).get('data').toJS()),
-      favez: state.getIn(['profile', 'favez']).get('data').toJS(),
-      selected: state.getIn(['profile', 'selected']),
-      loading: state.getIn(['profile', 'loading'])
+      // favez: state.getIn(['profile', 'favez']).get('data').toJS(),
+      // selected: state.getIn(['profile', 'selected']),
+      // loading: state.getIn(['profile', 'loading'])
     };
   }
 )(ProfileView);
