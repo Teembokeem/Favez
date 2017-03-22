619d57a349c33073735749e57d0a37a5a13e59a7
Teembokeem <myungtkim@gmail.com>
2017-03-22 04:25:23 -0700

    add base styling and html components to the line component for list show view

--- src/components/globals/fave/line.js
+++ src/components/globals/fave/line.js
@@ -12,9 +12,21 @@ import {
 
 function Line({fave}) {
   return (
-    <View>
-      <Text>{fave.name}</Text>
-      <Text>{fave.link}</Text>
+    <View style={styles.FaveContainer}>
+      <View style={styles.FaveHeader}>
+        {/*TODO: grab user image? <Image />*/}
+        <Text style={styles.FaveUsername}>@{fave.owner}: </Text>
+        <Text style={styles.FaveUserDescription}>{fave.description ? fave.description : ''}</Text>
+      </View>
+      <View
+        style={styles.FaveInfoContainer}
+      >
+        <Image style={styles.FaveInfoIcon} source={{uri: fave.image}}/>
+        <View style={styles.FaveInfoTextContainer}>
+          <Text style={styles.FaveInfoName}>{fave.name}</Text>
+          <Text style={styles.FaveInfoLink}>{fave.link}</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -30,24 +42,52 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 20
   },
-  fadeLayer: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    width: 355,
-    height: 400,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 0
-  },
-  ListBackground: {
-    width: 355,
-    height: 400,
-    position: 'absolute',
-    top: 0,
-    left: 0,
+  FaveContainer: {
     flex: 1,
-    zIndex: -1
+    backgroundColor: 'white',
+    padding: 17
   },
+  FaveHeader: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  FaveUsername: {
+    fontFamily: 'Hind-Bold',
+    fontSize: 18
+  },
+  FaveUserDescription: {
+    fontFamily: 'Hind-Light',
+    fontSize: 15
+  },
+  FaveInfoContainer: {
+    flex: 1,
+    paddingTop: 7,
+    paddingBottom: 7,
+    flexDirection: 'row'
+  },
+  FaveInfoIcon: {
+    width: 75,
+    height: 75,
+    borderColor: '#f8f8f8',
+    borderWidth: 0.8,
+    borderRadius: 7,
+    flex: 1
+  },
+  FaveInfoTextContainer: {
+    flex: 4,
+    paddingLeft: 10,
+    justifyContent: 'center'
+  },
+  FaveInfoName: {
+    fontFamily: 'Hind-Bold',
+    fontSize: 18,
+    lineHeight: 22
+  },
+  FaveInfoLink: {
+    fontFamily: 'Hind-Regular',
+    color: '#b8b8b8'
+  }
 });
 
 export default Line;
