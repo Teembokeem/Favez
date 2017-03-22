499ca5cb0cb3ba118bcc4dc91a96949ba7a701ae
Teembokeem <myungtkim@gmail.com>
2017-03-22 12:20:00 -0700

    add edit profile view

---
+++ src/modules/edit-profile/EditProfileView.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  View,
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet
+} from 'react-native';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
+import {Actions} from 'react-native-router-flux';
+import * as FaveActions from '../../redux/fave/faveActions';
+import * as ListActions from '../../redux/list/listActions';
+import * as UIActions from '../../redux/ui/uiActions';
+import Header from '../../components/globals/header/header';
+import EditProfileHeader from '../../components/edit-profile/editProfileHeader/editProfileHeader';
+import EditProfileForm from '../../components/edit-profile/editProfileForm/editProfileForm';
+
+const window = Dimensions.get('window');
+
+const EditProfileView = React.createClass({
+  propTypes: {},
+
+  componentWillMount() {
+    // this.props.dispatch(ListActions.getFullList());
+  },
+
+  submit(text) {
+    const {fave, selectedRadio} = this.props;
+    console.log('selected radio', selectedRadio, this.setMyList())
+    Object.assign(fave, {
+      name: fave.title,
+      description: text,
+      list_id: this.setMyList()[selectedRadio].id,
+      type: 1
+    });
+    this.props.dispatch(FaveActions.createFave(fave)).then((something, somethingelse) => {
+      console.log('TODO: fix this once, POST /favez is fixed', something, somethingelse);
+      this.props.dispatch(ListActions.getMyLists()).then(Actions.feedIndex);
+    });
+  },
+
+  render() {
+    const {user} = this.props;
+    const {favez, auth0} = user;
+    console.log('edit profile props', this.props);
+    return (
+      <View style={styles.container}>
+        <EditProfileHeader />
+        <ScrollView>
+          <Header title={'Edit Profile'}/>
+          <View style={styles.EditProfileFormContainer}>
+            <View style={styles.EditProfileImageChanger}>
+              <Text style={styles.EditProfileImage}>Image</Text>
+              <Text style={styles.EditProfileImageText}>Tap to change profile picture</Text>
+            </View>
+            <EditProfileForm
+              user={user}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 20,
+    alignItems: 'center'
+  }
+});
+
+export default EditProfileView;
