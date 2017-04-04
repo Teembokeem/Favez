a2a38735c21940b8423b9f7e13b42a8da204d8ba
Teembokeem <myungtkim@gmail.com>
2017-04-04 14:24:12 -0700

    add templateed more optinos view container

---
+++ src/modules/more-options/MoreOptionsViewContainer.js
@@ -0,0 +1,14 @@
+import {
+  connect
+} from 'react-redux';
+import MoreOptionsView from './MoreOptionsView';
+
+export default connect(
+  state => {
+    return {
+      tabs: state.getIn(['ui', 'listShow', 'tabs', 'set']).toJS(),
+      selectedTab: state.getIn(['ui', 'listShow', 'tabs', 'selected']),
+      loading: state.getIn(['listShow', 'loading']),
+    };
+  }
+)(MoreOptionsView);
