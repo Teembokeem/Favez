a8dd274ed29d0ac4d5b235b52224b00663a1f9e3
Teembokeem <myungtkim@gmail.com>
2017-04-06 16:23:42 -0700

    add helper method and case for SET_NEWLIST_OPTIONS action

--- src/redux/list/listReducer.js
+++ src/redux/list/listReducer.js
@@ -10,6 +10,7 @@ import {
   LIST_CREATE_REQUEST,
   LIST_CREATE_SUCCESS,
   LIST_CREATE_FAILURE,
+  LIST_SET_NEWLIST_OPTIONS,
   requestCreateList,
   requestGetMyLists
 } from './listActions';
@@ -20,6 +21,12 @@ const initialState = fromJS({
   myLists: [],
   collaborations: [],
   current: {},
+  options: {
+    description: '',
+    tags: [],
+    topics: [],
+    currTag: ''
+  },
   loading: true
 });
 
@@ -48,9 +55,6 @@ export default function ListReducer(state = initialState, action = {}) {
       return state
         .set('loading', false)
         .set('myLists', action.payload.data);
-    case LIST_MYLIST_FAILURE:
-      console.log('ERROR', state, action);
-      return state.set('ERROR', action);
     case LIST_CREATE_REQUEST:
       return loop(
         state.set('loading', true),
@@ -60,10 +64,42 @@ export default function ListReducer(state = initialState, action = {}) {
       console.log('SUCCESS', state, action);
       return state
         .set('loading', false);
+    case LIST_SET_NEWLIST_OPTIONS:
+      console.log('setting new list options: ', state.get('options'), action.payload);
+      let key = Object.keys(action.payload)[0];
+      return insertOptionParams(state, state.get('options'), key, action.payload[key]);
+    case LIST_MYLIST_FAILURE:
     case LIST_CREATE_FAILURE:
       console.log('ERROR', state, action);
       return state.set('ERROR', action);
     default:
       return state;
+  }
+}
+
+function insertOptionParams(state, obj, prop, values) {
+  switch (prop) {
+    case 'description':
+    case 'currTag':
+      return state
+        .setIn(['options', prop], values);
+    case 'topics':
+      return state
+        .setIn(['options', prop], state.getIn(['options', prop]).concat(values))
+    case 'tags':
+      console.log('tags', state.getIn(['options', prop]), values);
+      if (typeof values === 'number') {
+        console.log('deleting!!');
+        let newArr = state.getIn(['options', prop]).splice(values, 1);
+        return state
+          .setIn(['options', prop], newArr)
+          .setIn(['options', 'currTag'], '');
+      } else {
+        return state
+          .setIn(['options', prop], state.getIn(['options', prop]).concat(values))
+          .setIn(['options', 'currTag'], '');
+      }
+    default:
+      return null;
   }
 }
