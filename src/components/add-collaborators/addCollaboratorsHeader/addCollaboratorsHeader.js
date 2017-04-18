09fbf456377388524fe61c3aa453238fad20c203
Teembokeem <myungtkim@gmail.com>
2017-04-18 16:41:16 -0700

    add add collaborators header

---
+++ src/components/add-collaborators/addCollaboratorsHeader/addCollaboratorsHeader.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Actions} from 'react-native-router-flux';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 30 : 0;
+const window = Dimensions.get('window');
+
+const AddCollaboratorsHeader = React.createClass({
+
+  render() {
+    console.log(this.props);
+    return (
+    <View style={styles.headerContainer}>
+        <TouchableOpacity
+            style={styles.headerLeftButton}
+            onPress={Actions.pop}
+        >
+            <IoniconIcon style={styles.headerLeftButtonIcon} name='md-arrow-round-back' />
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.headerCenter}
+        >
+            {/*<FontAwesomeIcon style={styles.searchBarIcon} name="search"/>*/}
+
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.headerRightButton}
+        >
+          <FAIcon style={styles.headerRightIcon} name='address-book'/>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.headerRightButton}
+        >
+          <FAIcon style={styles.headerRightIcon} name='search'/>
+        </TouchableOpacity>
+    </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: window.width,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: STATUSBAR_HEIGHT,
+    height: APPBAR_HEIGHT + STATUSBAR_HEIGHT
+  },
+  headerLeftButton: {
+    flex: 1
+  },
+  headerLeftButtonIcon: {
+    // width: 35,
+    fontSize: 30,
+    color: '#000000',
+    alignSelf: 'center'
+  },
+  headerCenter: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    height: 35,
+    flex: 5
+  },
+  headerRightButton: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  headerRightIcon: {
+    // width: 35,
+    fontSize: 25,
+    color: '#000000'
+  },
+  headerRightCancel: {
+    fontFamily: 'Hind-Medium',
+    fontSize: 17
+  }
+
+});
+
+export default AddCollaboratorsHeader;
