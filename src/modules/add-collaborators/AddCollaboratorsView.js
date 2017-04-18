2322b8405f9f036f2fa6d9f101358f5b5e715b65
Teembokeem <myungtkim@gmail.com>
2017-04-18 16:42:10 -0700

    add boiler for add collaborators view

--- src/modules/add-collaborators/AddCollaboratorsView.js
+++ src/modules/add-collaborators/AddCollaboratorsView.js
@@ -2,54 +2,27 @@ import React from 'react';
 import * as UIActions from '../../redux/ui/uiActions';
 import {
   View,
+  Text,
   StyleSheet
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import * as FaveActions from '../../redux/fave/faveActions';
-import FavezBrowser from '../../components/globals/favezBrowser/favezBrowser';
+import AddCollaboratorsHeader from '../../components/add-collaborators/addCollaboratorsHeader/addCollaboratorsHeader';
 
 const AddFaveBrowseView = React.createClass({
   propTypes: {},
 
   componentWillMount() {
-    // [TD2d] you have access to this.props here
-    const {title, url} = this.props.browser;
-    if (this.props.viewList) {
-      return;
-    } else {
-      this.props.dispatch(UIActions.setBrowserInitialState());
-    }
-  },
-
-  setBrowserInfo(url, title) {
-    if (url) {
-      this.props.dispatch(UIActions.setBrowserInfo(url, title));
-    } else {
-      return;
-    }
-  },
-
-  scrapeUrl(url) {
-    return this.props.dispatch(UIActions.scrapeUrl(url));
-  },
-
-  setNewFave(fave) {
-    return this.props.dispatch(FaveActions.setNewFave(fave)).then(() => {
-      this.props.dispatch(UIActions.setBrowserInitialState()).then(Actions.addFaveForm());
-    });
   },
 
   render() {
-    const {browser, viewList} = this.props;
+    const {lists} = this.props;
     return (
       <View style={styles.container}>
-        <FavezBrowser
-          browser={browser}
-          showViewList={viewList}
-          scrapeUrl={this.scrapeUrl}
-          setBrowserInfo={this.setBrowserInfo}
-          setNewFave={this.setNewFave}
-        />
+        <AddCollaboratorsHeader/>
+        <View>
+          <Text style={styles.Header}>Hello World</Text>
+        </View>
       </View>
     );
   }
@@ -58,6 +31,9 @@ const AddFaveBrowseView = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  Header: {
+    fontSize: 30
   }
 });
 
