d31275e5f8f289dc728e1afc5ac15b49f10a28b2
Teembokeem <myungtkim@gmail.com>
2017-04-18 16:42:41 -0700

    add general data bindings for add collaborators container

--- src/modules/add-collaborators/AddCollaboratorsViewContainer.js
+++ src/modules/add-collaborators/AddCollaboratorsViewContainer.js
@@ -1,18 +1,12 @@
-
 import {
   connect
 } from 'react-redux';
-import AddFaveBrowseView from './AddFaveBrowseView';
+import AddCollaboratorsView from './AddCollaboratorsView';
 
 export default connect(
   state => {
-    console.log(state.getIn(['ui', 'browser']))
     return {
-      lists: state.getIn(['list', 'all']),
-      browser: state.getIn(['ui', 'browser']).toJS()
-      // headerContextMenu: state.getIn(['feed', 'header'])
-      // cards: state.getIn(['feed', 'cards']).get('data').toJS(),
-      // loading: state.getIn(['feed', 'loading'])
+      lists: state.getIn(['list', 'all'])
     };
   }
-)(AddFaveBrowseView);
+)(AddCollaboratorsView);
