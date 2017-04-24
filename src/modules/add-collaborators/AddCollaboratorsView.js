c2f97fd818537560955a5f184e23f4d011fda2b2
Teembokeem <myungtkim@gmail.com>
2017-04-24 16:18:39 -0700

    add search method for collaborators on add collaborators view

--- src/modules/add-collaborators/AddCollaboratorsView.js
+++ src/modules/add-collaborators/AddCollaboratorsView.js
@@ -5,8 +5,7 @@ import {
   StyleSheet
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
-import * as UIActions from '../../redux/ui/uiActions';
-import * as FaveActions from '../../redux/fave/faveActions';
+import * as UserActions from '../../redux/user/userActions';
 import AddCollaboratorsHeader from '../../components/add-collaborators/addCollaboratorsHeader/addCollaboratorsHeader';
 import Header from '../../components/globals/header/header';
 import Thumbnail from '../../components/globals/thumbnail/thumbnail';
@@ -17,14 +16,26 @@ const AddFaveBrowseView = React.createClass({
   componentWillMount() {
   },
 
+  search() {
+    console.log('search method');
+    this.props.dispatch(UserActions.findCollaborators())
+      .then((res) => {
+        console.log('post user action find collaborators', res);
+        Actions.searchCollaborators();
+      })
+      .catch((err) => console.log('Error', err));
+  },
   render() {
     const {} = this.props;
     return (
       <View style={styles.container}>
-        <AddCollaboratorsHeader/>
+        <AddCollaboratorsHeader
+          search={this.search}
+        />
         <Header title='Collaborators'/>
         <ScrollView contentContainerStyle={styles.CollaboratorsContainer}>
           <Thumbnail />
+          {/*TODO: send private method, and icon for different actions.*/}
         </ScrollView>
       </View>
     );
