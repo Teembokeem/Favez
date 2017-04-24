94681f4127bea7a64a1bf03c69f2980cdd2e08aa
Teembokeem <myungtkim@gmail.com>
2017-04-24 11:36:02 -0700

    finish base styling for new collaborator. Prep for adding search modal

--- src/modules/add-collaborators/AddCollaboratorsView.js
+++ src/modules/add-collaborators/AddCollaboratorsView.js
@@ -6,6 +6,7 @@ import {
   Image,
   StyleSheet
 } from 'react-native';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Actions} from 'react-native-router-flux';
 import * as FaveActions from '../../redux/fave/faveActions';
 import AddCollaboratorsHeader from '../../components/add-collaborators/addCollaboratorsHeader/addCollaboratorsHeader';
@@ -24,11 +25,25 @@ const AddFaveBrowseView = React.createClass({
         <Header title='Collaborators'/>
         <View style={styles.CollaboratorsContainer}>
           <View style={styles.CollaboratorContainer}>
-            <View style={styles.CollaboratorImageContainer}>
-              <Image />
+            <View
+              style={styles.CollaboratorImageContainer}
+            >
+              <Image style={styles.CollaboratorImage}/>
             </View>
-            <View></View>
-            <View></View>
+            <View
+              style={styles.CollaboratorInfoContainer}
+            >
+              <View style={styles.CollaboratorInfoNameContainer}>
+                <Text style={styles.CollaboratorInfoName}>@{'Pewdiepie'}</Text>
+                {/*<Text style={styles.CollaboratorInfoNameIcon}>Star</Text>*/}
+              </View>
+              <View style={styles.CollaboratorInfoTagContainer}>
+                <Text style={styles.CollaboratorInfoTag}>PewdiePie Official</Text>
+              </View>
+            </View>
+            <View style={styles.CollaboratorDeleteContainer}>
+              <MCIcon style={styles.CollaboratorDeleteIcon} name='delete'/>
+            </View>
           </View>
         </View>
       </View>
@@ -40,8 +55,63 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  Header: {
-    fontSize: 30
+  CollaboratorsContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    borderTopWidth: 0.3,
+    borderColor: '#a8a8a8'
+  },
+  CollaboratorContainer: {
+    flex: 1,
+    paddingLeft: 18,
+    alignItems: 'center',
+    paddingRight: 18,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    maxHeight: 70
+  },
+  CollaboratorImageContainer: {
+    flex: 1
+  },
+  CollaboratorImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'grey'
+  },
+  CollaboratorInfoContainer: {
+    flex: 6,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingLeft: 18,
+    flexDirection: 'column'
+  },
+  CollaboratorInfoNameContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 15
+  },
+  CollaboratorInfoName: {
+    color: '#0076ff',
+    fontFamily: 'Hind-Bold',
+    fontSize: 15
+  },
+  CollaboratorInfoTagContainer: {
+    flex: 1,
+    paddingBottom: 14
+  },
+  CollaboratorInfoTag: {
+    color: '#a8a8a8',
+    fontFamily: 'Hind-SemiBold',
+    fontSize: 13
+  },
+  CollaboratorDeleteContainer: {
+    flex: 1,
+    alignItems: 'flex-end'
+  },
+  CollaboratorDeleteIcon: {
+    fontSize: 27,
+    color: 'red'
   }
 });
 
