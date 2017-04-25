5370ab25ec1f9b94f77d55ff7408f846640370d0
Teembokeem <myungtkim@gmail.com>
2017-04-25 13:27:57 -0700

    add append invitee method and boolean for displaying whether invites have been sent.

--- src/modules/search-collaborators/SearchCollaboratorsView.js
+++ src/modules/search-collaborators/SearchCollaboratorsView.js
@@ -10,6 +10,7 @@ import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Actions} from 'react-native-router-flux';
 import * as UIActions from '../../redux/ui/uiActions';
 import * as FaveActions from '../../redux/fave/faveActions';
+import * as ListActions from '../../redux/list/listActions';
 import SearchCollaboratorsHeader from '../../components/search-collaborators/searchCollaboratorsHeader/searchCollaboratorsHeader';
 import SearchCollaboratorsSearchBar from '../../components/search-collaborators/searchCollaboratorsSearchBar/searchCollaboratorsSearchBar';
 import Header from '../../components/globals/header/header';
@@ -21,23 +22,41 @@ const SearchCollaboratorsView = React.createClass({
   componentWillMount() {
   },
 
+  appendInvitee(invitee) {
+    console.log('does he exist?: ', this.props.inviteList.indexOf(invitee) === -1)
+    return this.props.inviteList.indexOf(invitee) === -1
+    ? this.props.dispatch(ListActions.modifyInviteList(invitee, true))
+    : this.props.dispatch(ListActions.modifyInviteList(invitee, false));
+  },
+
   render() {
     console.log('GOT PROPS', this.props);
-    const {collaborators} = this.props;
+    const {collaborators, inviteList} = this.props;
     return (
       <View style={styles.container}>
         <SearchCollaboratorsHeader />
         <SearchCollaboratorsSearchBar />
         <ScrollView contentContainerStyle={styles.CollaboratorsContainer}>
           {collaborators.map((collaborator, idx) => {
-            return (
-              <Thumbnail
-                key={'collaborator ' + idx}
-                collaborator={collaborator}
-                method={Actions.pop}
-                iconSpecs={{pack: 'FAIcon', name: 'inbox'}}
-              />
-            );
+            if (inviteList.indexOf(collaborator) > -1) {
+              return (
+                <Thumbnail
+                  key={'collaborator ' + idx}
+                  collaborator={collaborator}
+                  method={this.appendInvitee}
+                  iconSpecs={{pack: 'MCIcon', name: 'check-circle'}}
+                />
+              );
+            } else {
+              return (
+                <Thumbnail
+                  key={'collaborator ' + idx}
+                  collaborator={collaborator}
+                  method={this.appendInvitee}
+                  iconSpecs={{pack: 'FAIcon', name: 'inbox'}}
+                />
+              );
+            }
           })}
         </ScrollView>
       </View>
