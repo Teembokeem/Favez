67f2cd630edb06630a91a3acec3221083f6d4fb0
Teembokeem <myungtkim@gmail.com>
2017-04-25 14:41:05 -0700

    add invitelist to creat elist view container

--- src/modules/create-list/CreateListViewContainer.js
+++ src/modules/create-list/CreateListViewContainer.js
@@ -7,7 +7,8 @@ export default connect(
   state => {
     return {
       lists: state.getIn(['list', 'all']),
-      options: state.getIn(['list', 'options']).toJS()
+      options: state.getIn(['list', 'options']).toJS(),
+      inviteList: state.getIn(['list', 'inviteList']).toJS()
     };
   }
 )(CreateListView);
