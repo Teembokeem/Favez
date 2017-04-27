a0a817082ee4ef72c6cf0d422b8b8ef617775b9e
Teembokeem <myungtkim@gmail.com>
2017-04-27 16:44:20 -0700

    add exportable notification service method for retrieving invites to self

--- src/services/notification.js
+++ src/services/notification.js
@@ -2,4 +2,8 @@ import {get, post, put, delete} from '../utils/api';
 
 export async function notificationGetMyNotifs() {
   return get('/alerts/self');
-};
+}
+
+export async function notificationGetInvites() {
+  return get('/users/collaborate/invited');
+}
