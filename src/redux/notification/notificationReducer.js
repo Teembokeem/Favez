5fa80c3d3ff9d9deefec907a9c858173f1aa25c3
Teembokeem <myungtkim@gmail.com>
2017-04-27 16:44:05 -0700

    add action delegator in notifciation reducer

--- src/redux/notification/notificationReducer.js
+++ src/redux/notification/notificationReducer.js
@@ -4,12 +4,17 @@ import {
   NOTIFICATION_GET_MY_NOTIFS_REQUEST,
   NOTIFICATION_GET_MY_NOTIFS_SUCCESS,
   NOTIFICATION_GET_MY_NOTIFS_FAILURE,
-  requestGetNotifs
+  NOTIFICATION_GET_MY_INVITES_REQUEST,
+  NOTIFICATION_GET_MY_INVITES_SUCCESS,
+  NOTIFICATION_GET_MY_INVITES_FAILURE,
+  requestGetNotifs,
+  requestGetInvites
 } from './notificationActions';
 
 // Initial state
 const initialState = fromJS({
   myNotifs: [],
+  myInvites: [],
   // current: {},
   loading: true,
   error: {}
@@ -19,7 +24,7 @@ const initialState = fromJS({
 export default function NotificationReducer(state = initialState, action = {}) {
   switch (action.type) {
     case NOTIFICATION_GET_MY_NOTIFS_REQUEST:
-      console.log('Notification GET_MY_Notifs request');
+      console.log('Notification GET_MY_NOTIFS request');
       return loop(
         state.set('loading', true),
         Effects.promise(() => requestGetNotifs(action.payload))
@@ -28,10 +33,24 @@ export default function NotificationReducer(state = initialState, action = {}) {
       console.log('SUCCESS', action);
       return state
         .set('loading', false)
-        .set('myNotifs', action.payload.data);
+        .set('myNotifs', action.payload);
+    case NOTIFICATION_GET_MY_INVITES_REQUEST:
+      console.log('Notification GET_MY_INVITES request');
+      return loop(
+        state.set('loading', true),
+        Effects.promise(() => requestGetInvites(action.payload))
+      );
+    case NOTIFICATION_GET_MY_INVITES_SUCCESS:
+      console.log('SUCCESS', action);
+      return state
+        .set('loading', false)
+        .set('myInvites', action.payload);
     case NOTIFICATION_GET_MY_NOTIFS_FAILURE:
+    case NOTIFICATION_GET_MY_INVITES_FAILURE:
       console.log('ERROR', action);
-      return state.set('ERROR', action);
+      return state
+        .set('loading', false)
+        .set('ERROR', action);
     default:
       return state;
   }
