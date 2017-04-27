eddcd34f1e4fb8bd2bdbed3cf02eb20f039b34d2
Teembokeem <myungtkim@gmail.com>
2017-04-27 15:49:21 -0700

    add get notifications action sheet barebones

---
+++ src/redux/notification/notificationActions.js
@@ -0,0 +1,21 @@
+import {
+  notificationGetMyNotifs
+} from '../../services/notifications';
+
+// Actions
+export const NOTIFICATION_GET_MY_NOTIFS_REQUEST = 'NOTIFICATION_GET_MY_NOTIFS_REQUEST';
+export const NOTIFICATION_GET_MY_NOTIFS_SUCCESS = 'NOTIFICATION_GET_MY_NOTIFS_SUCCESS';
+export const NOTIFICATION_GET_MY_NOTIFS_FAILURE = 'NOTIFICATION_GET_MY_NOTIFS_FAILURE';
+
+// Action creators
+export async function getNotifs() {
+  return {
+    type: NOTIFICATION_GET_MY_NOTIFS_REQUEST
+  };
+}
+
+export async function requestGetNotifs() {
+  return await notificationGetMyNotifs()
+    .then((res) => ({type: NOTIFICATION_GET_MY_NOTIFS_SUCCESS, payload: res}))
+    .catch((err) => ({type: NOTIFICATION_GET_MY_NOTIFS_FAILURE, payload: err}));
+}
