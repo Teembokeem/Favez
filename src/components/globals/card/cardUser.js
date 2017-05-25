99bbac8938e904321a3672e9e92a3464b2e3fd31
ajchan11 <ajchan11@gmail.com>
2017-05-25 11:07:59 -0700

    clean up cards and default avatar

--- src/components/globals/card/cardUser.js
+++ src/components/globals/card/cardUser.js
@@ -8,32 +8,30 @@ import {
   Image,
   View
 } from 'react-native';
+import {toDuration} from '../../../utils/timeUtils.js'
 
-function CardUser({card, time, showProfile,onUserAction,userActionData}) {
-  console.log("dcvf",userActionData);
+function CardUser({card, showProfile, onUserAction, userActionData}) {
+
   function renderUserActionIcon() {
-
     switch(userActionData.type) {
       case "follow_unfollow":
         return (
-
           <IoniconIcon style={styles.cardUserSubscribe} name={userActionData.data ? 'md-person' : 'md-person-add'}/>
       );
-
     }
-
   };
+
   const user = (card.user && card.user.length) ? card.user[0] : {f1: 'Unknown'}
   return (
       <View style={[styles.cardUserContainer]}>
-          {/*<Image source={{uri: user.avatar}} style={[styles.cardUserImage]} />*/}
+          <Image style={[styles.cardUserImage]} source={user['f3'] ? {uri: user['f3']} : require('../../../../images/default_avatar.png')}/>
           <View
             style={styles.cardUserInfoColumn}>
             <Text style={[styles.cardUserName]} onPress={showProfile}>{'@' + user.f1}</Text>
-            <Text style={styles.cardUserAgo}>{time}</Text>
+            <Text style={styles.cardUserAgo}>{`${toDuration(card.created)} ago`}</Text>
           </View>
 
-              <TouchableOpacity      onPress={() => onUserAction() }>
+              <TouchableOpacity onPress={() => onUserAction() }>
                 {renderIf(userActionData.type)(renderUserActionIcon())}
 {/*}                <IoniconIcon style={styles.cardUserSubscribe} name={followed ? 'md-person' : 'md-person-add'}/> */}
               </TouchableOpacity>
